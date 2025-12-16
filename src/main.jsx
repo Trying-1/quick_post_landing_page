ff22a4e1dfb0ea2e147f232d7addf68704c13eff
Trying-1 <tryingbait@gmail.com>
2025-12-16 10:34:26 +0530

    static files for privcy policy and account deletion for play console urls

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import PrivacyPolicy from './pages/PrivacyPolicy.jsx'
-import AccountDeletion from './pages/AccountDeletion.jsx'
 import './index.css'
+
+// Redirect components to static HTML files
+const PrivacyPolicyRedirect = () => {
+  React.useEffect(() => {
+    window.location.href = '/privacy-policy.html'
+  }, [])
+  return null
+}
+
+const AccountDeletionRedirect = () => {
+  React.useEffect(() => {
+    window.location.href = '/account-deletion.html'
+  }, [])
+  return null
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/account-deletion" element={<AccountDeletion />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicyRedirect />} />
+        <Route path="/account-deletion" element={<AccountDeletionRedirect />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
